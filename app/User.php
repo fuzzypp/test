5de9d82af7c026addcb25e221cdca0b5a481cd29
zdp <465201770@qq.com>
2017-10-28 17:09:52 +0000

    dev 1.0

--- app/User.php
+++ app/User.php
@@ -27,6 +27,6 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'passwoqrd',
+        'passwoqrd128888883',
     ];
 }
